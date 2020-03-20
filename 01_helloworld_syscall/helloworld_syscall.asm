5bb4a62ea9c243874c5166a24fdd7c4ca6518e1c
randyfortier <randy.fortier@gmail.com>
2020-03-20 14:07:37 -0400

    Two hello world examples added

--- 01_helloworld_syscall/helloworld_syscall.asm
+++ 01_helloworld_syscall/helloworld_syscall.asm
@@ -1,2 +1,25 @@
 ; this example will be a simple hello world example, using Linux system calls
 ; more info about Linux system calls:  http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64
+
+global main
+
+section .data
+    ; this is where your global variables go
+    message db "Hello from Linux!", 0ah, 0dh  ; 19 bytes/characters
+
+
+section .text 
+    ; this is where your instructions go
+
+main:
+    ; output our message, using sys_write()
+    mov rax, 1          ; sys_write()
+    mov rdi, 1          ; standard output (cout)
+    mov rsi, message    ; pointer to our message
+    mov rdx, 19         ; how many bytes to write
+    syscall             ; jump to the kernel to execute sys_write()
+
+    ; exit from the program
+    mov rax, 60         ; sys_exit()
+    mov rdi, 0          ; return 0 (C++), exit(0) (C)
+    syscall
--- 02_helloworld_libc/helloworld.asm
+++ 02_helloworld_libc/helloworld.asm
@@ -1,2 +1,27 @@
 ; this example is another hello world example, but this time using the C library (libc)
 
+extern printf
+
+global main
+
+section .text
+
+main:
+    ; printf("Hello from LibC!")   <- basically, this
+    ; printf("%s", message)        <- more accurately, this
+    ; arguments:  rdi, rsi, rax
+    ; return value: rax
+    mov rdi, format 
+    mov rsi, message 
+    mov rax, 0
+    call printf
+
+    ; exit(0) or return 0
+    mov rax, 0
+    ret
+
+
+section .data
+    format db "%s", 0ah, 0dh, 0
+    message db "Hello from LibC!", 0
+    courseCode dq 2050
