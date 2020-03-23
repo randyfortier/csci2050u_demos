7bc44cf30096842bb7d4af7294dd707756062cfc
randyfortier <randy.fortier@gmail.com>
2020-03-23 10:35:36 -0400

    Added modification to the makefile to potentially fix seg faults on some machines

--- 02_helloworld_libc/helloworld.asm
+++ 02_helloworld_libc/helloworld.asm
@@ -15,14 +15,15 @@ main:
     mov rdi, format             ; argument #1 ("%s")
     mov rsi, message            ; argument #2 ("Hello from LibC!")
     mov rax, 0                  ; how many floating point arguments are there to printf()?
-    sub rsp, 8                  ; must be 16-byte aligned (more on this later)
-    call printf
+    push rbx                    ; must be 16-byte aligned (more on this later)
+    call printf                 ; adds return address (8-bytes, 64-bits) to the stack
+    pop rbx                     ; remove the unnecessary value pushed to the stack before printf()
 
     ; exit(0)
     mov rax, 0
     call exit
 
 section .data
-    format db "%s", 0ah, 0dh, 0
-    message db "Hello from LibC!", 0
+    format     db "%s", 0ah, 0dh, 0
+    message    db "Hello from LibC!", 0
     courseCode dq 2050
